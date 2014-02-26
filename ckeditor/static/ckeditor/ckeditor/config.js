1e96483ecd1e519759badbfa7f94897a528bb57c
maxvyaznikov <vmn@siterra.org>
2014-02-26 11:22:20 +0300

    font changes

--- ckeditor/static/ckeditor/ckeditor/config.js
+++ ckeditor/static/ckeditor/ckeditor/config.js
@@ -65,5 +65,18 @@ CKEDITOR.editorConfig = function( config ) {
         'toolbar,' +
         'undo,' +
         'wysiwygarea';
-    
+
+    config.font_names = 'Arial/Arial, sans-serif;' +
+                        'ArialRoundedMTProCyr-Bold/ArialRoundedMTProCyr-Bold, Arial, sans-serif;'+
+                        'HeliosWeb/HeliosWeb, Arial, sans-serif;'+
+//                        'Comic Sans MS/Comic Sans MS, cursive;' +
+//                        'Courier New/Courier New, Courier, monospace;' +
+//                        'Georgia/Georgia, serif;' +
+//                        'Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;' +
+//                        'Tahoma/Tahoma, Geneva, sans-serif;' +
+//                        'Times New Roman/Times New Roman, Times, serif;' +
+//                        'Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;' +
+//                        'Verdana/Verdana, Geneva, sans-serif'+
+    '';
+    config.forcePasteAsPlainText  = true;
 };
